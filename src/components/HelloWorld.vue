7b11ec9cfc8d300e0251430d77eaa18eb1295421
Sandeep-777 <sandeepsthapit@gmail.com>
2020-11-05 15:20:01 +0545

    test

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    <h1>AngrySandy</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mattis ligula feugiat nunc hendrerit iaculis. Sed pharetra vulputate erat eu iaculis. Proin malesuada aliquet dolor nec pulvinar. Nullam ex justo, gravida quis efficitur consectetur, fermentum sit amet justo. Mauris pharetra eleifend nisi vel mollis. Morbi lacus ligula, pharetra in lacinia quis, sollicitudin non erat. Nunc sollicitudin vestibulum ligula ac tempor. Nunc ac interdum tellus, a mollis risus. Integer cursus lectus ante, et congue orci vehicula vitae.</p>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello {
+  padding: 20px 60px;
+}
+p {
+  color: darkcyan;
+}
+</style>
